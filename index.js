2dccd553b55a47c0a62ea6ce0e6cf4090d364599
originalang <angeorge200@gmail.com>
2019-05-09 13:11:57 -0400

    Add basic game start mechanics

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const port = process.env.PORT || 3000;
 
 let users = [];
 let connections = []
+let games = {};
+
+let currentGameCode = 111111;
 
 server.listen(port);
 console.log('Server Running...')
@@ -24,5 +27,25 @@ io.sockets.on('connection', (socket) => {
     socket.on('disconnect', () => {
         connections.splice(connections.indexOf(socket), 1);
         console.log(`Disconnected: ${connections.length} sockets connected`);
+    });
+
+    socket.on('new game', () => {
+        games[currentGameCode] = {
+            'connections': [socket],
+            'users': []
+        }
+
+        socket.emit('game code', currentGameCode);
+        currentGameCode++;
+    });
+
+    socket.on('join game', (code) => {
+        if (games.hasOwnProperty(code)) {
+            games[code].connections.push(socket);
+            socket.emit('game found');
+            socket.emit('game code', code);
+        } else {
+            socket.emit('game not found');
+        }
     });
 });
